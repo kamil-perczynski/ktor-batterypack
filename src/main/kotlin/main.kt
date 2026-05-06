371c2c24c01a249145de7869c0ad19d532eb3c6b
kamil-perczynski <kamil.perczynski.95@gmail.com>
2026-05-06 20:43:14 +0200

    Init commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,8 @@
+package io.github.kperczynski
+
+import io.ktor.server.engine.*
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
