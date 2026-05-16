3417d4ee0d0e195f89f801ad1adc301556fb4b0b
kamil-perczynski <kamil.perczynski.95@gmail.com>
2026-05-16 20:46:14 +0200

    style: remove unused imports and standardize on singleton annotation

--- src/main/kotlin/domain/user/UserEventPublisher.kt
+++ src/main/kotlin/domain/user/UserEventPublisher.kt
@@ -2,13 +2,13 @@ package io.github.kperczynski.domain.user
 
 import io.lettuce.core.XAddArgs
 import io.lettuce.core.api.StatefulRedisConnection
-import org.koin.core.annotation.Single
+import org.koin.core.annotation.Singleton
 import org.slf4j.LoggerFactory
 import tools.jackson.databind.json.JsonMapper
 
 private val log = LoggerFactory.getLogger(UserEventPublisher::class.java)
 
-@Single
+@Singleton
 class UserEventPublisher(
     private val connection: StatefulRedisConnection<String, String>,
     private val jsonMapper: JsonMapper
