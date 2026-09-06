fa9ec0761bcb82694179964bd746be6ee9eb5d57
kamil-perczynski <kamil.perczynski.95@gmail.com>
2026-09-06 17:01:28 +0200

    chore: publish version catalog and fix docker build context

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     alias(libs.plugins.kotlin.jvm)
+    `version-catalog`
+    `maven-publish`
 }
 
 group = "io.github.kperczynski"
@@ -8,4 +10,29 @@ version = "0.0.0-SNAPSHOT"
 allprojects {
     group = rootProject.group
     version = rootProject.version
+}
+
+catalog {
+    versionCatalog {
+        from(files("gradle/libs.versions.toml"))
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["versionCatalog"])
+            artifactId = "ktor-batterypack-versions-catalog"
+        }
+    }
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/kamil-perczynski/ktor-batterypack")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
 }
